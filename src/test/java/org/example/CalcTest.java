9ad52721cff2880352f2f0796023e5a5f92c739c
Shivani Shukla <shuklashivani2501@gmail.com>
2025-02-28 17:31:42 +0530

    testing

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -1,13 +1,8 @@
 package org.example;
-
-//public class test {
-//}
 import org.junit.jupiter.api.Test;
-
-import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
-class test {
+class CalcTest {
 
     @Test
     void testFactorial() {
